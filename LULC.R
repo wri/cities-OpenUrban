9695eed5e7c52cb69de10e6eb8bcdd07ac496186
Work <WRI@Elizabeths-iMac.ht.home>
2023-08-18 15:17:47 -0400

    completed open space

--- LULC.R
+++ LULC.R
@@ -4,6 +4,7 @@
 library(here)
 library(tidyverse)
 library(sf)
+library(terra)
 library(osmdata)
 library(rgee)
 # library(reticulate)
@@ -15,6 +16,8 @@ library(rgee)
 reticulate::use_condaenv("rgee")
 
 # Initialize Earth Engine and GD
+# ee_Authenticate("ejwesley")
+ee_Initialize("ejwesley", drive = TRUE)
 
 # City boundary -----------------------------------------------------------
 
@@ -22,7 +25,7 @@ reticulate::use_condaenv("rgee")
 # cities <- st_read("data/Smart_Surfaces_metro_areas/smart_surfaces_urban_areas.shp")
 
 # Choose city of interest, use underscores instead of spaces
-# city <- "New_Orleans"
+# city_name <- "Los_Angeles"
 
 # Use LA AOI for testing purposes
 city <- st_read("data/AOI.geojson")
@@ -37,6 +40,8 @@ bb <- city %>%
   st_buffer(dist = 804.672) %>% 
   st_bbox() 
 
+bb_ee <-  sf_as_ee(st_as_sfc(bb))
+
 # ggplot() +
 #   geom_sf(data = bb) +
 #   geom_sf(data = city)
@@ -49,10 +54,10 @@ epsg <- 26945
 # ESA Worldcover ----------------------------------------------------------
 
 # Read ESA land cover (2021)
-esa <- ee_ImageCollection('ESA/WorldCover/v200')
+esa <- ee$ImageCollection('ESA/WorldCover/v200')
 
 # Clip to city of interest
-esa_city <- esa$filterBounds(bb_geom)
+esa_city <- esa$filterBounds(bb_ee)
 
 # ImageCollection to image
 esa_city <- esa_city$first()
@@ -61,50 +66,46 @@ esa_city <- esa_city$first()
 
 ## [ND, Tree, Shrubland, Grassland, Cropland, Built up, Bare/sparse vegetation,
 ##  Snow/ice, Permanent water bodies, Herbaceous wetland, Mangroves, Moss and lichen]
-FROM = [0, 10, 20, 30, 40, 50, 60, 80, 70, 90, 95, 100]
+FROM = c(0, 10, 20, 30, 40, 50, 60, 80, 70, 90, 95, 100)
 
 ## [ND, Green, Green, Green, Green, Built up, Barren, Water, Water, Water, Water, Barren]
-TO = [0, 1, 1, 1, 1, 2, 3, 4, 4, 4, 4, 3]
+TO = c(0, 1, 1, 1, 1, 2, 3, 4, 4, 4, 4, 3)
 
 esa_city_rm = esa_city$remap(FROM, TO)
 
 # Resample raster from 10-m to 1-m
-esa_1m = esa_city.reproject(crs = "EPSG:"+proj, scale = 1)
+# GEE performs nearest neighbor resampling by default
+# https://developers.google.com/earth-engine/guides/resample
+esa_1m = esa_city$reproject(crs = paste0("EPSG:", epsg), scale = 1)
 
-# Export to Google Drive
-geemap.ee_export_image_to_drive(
-  esa_1m.toByte(), # use toByte() to reduce file size 
-  scale = 1, # 10 for native resolution, 50 for smaller file size 
-  description = 'ESA_1m',
-  region = bb_geom,
-  maxPixels = 5000000000
-)
+# Map$addLayer(
+#   eeObject = esa_1m)
 
-# Create city folder
-out_dir = os.getcwd()
-city_folder = out_dir + "/data/" + city
-os.makedirs(city_folder)
+# Create city folder to store data
+path <- paste0("data/", city_name)
 
-################
-## Download from Google Drive to city folder
-################
+if (!dir.exists(path)){
+  dir.create(path)
+} else{
+  print("dir exists")
+}
 
-################
-## Need help with this loop, create variables for unknown # of tifs
-# Read local raster
+# Download from Google Drive to city folder
+esa_img <- ee_as_rast(esa_1m,
+                      region = bb_ee,
+                      dsn = "ESA_1m",
+                      via = "drive",
+                      scale = 1,
+                      maxPixels = 10e10,
+                      lazy = TRUE) 
 
-img_list = glob.glob(city_folder + '/*ESA_1m*')
-# print(img_list)
-
-esa_1 = rasterio.open(img_list[0])
-# esa_2 = rasterio.open(img_list[1])
-
-print(esa_1.crs)
-
+esa <- esa_img %>% 
+  ee_utils_future_value() %>% 
+  writeRaster(paste0(path, "/ESA_1m.tif"))
 
 # Open space --------------------------------------------------------------
 
-# get roads from OSM
+# get open space from OSM
 open_space1 <- opq(bb) %>% 
   add_osm_feature(key = 'leisure',
                   value = c('park', 'nature_reserve', 'common', 
@@ -122,6 +123,25 @@ open_space <- c(open_space1, open_space2)
 open_space <- open_space$osm_polygons %>% 
   bind_rows(st_cast(open_space$osm_multipolygons, "POLYGON")) 
 
-ggplot() +
-  geom_sf(data = st_as_sfc(bb)) +
-  geom_sf(data = open_space4)
+# Reproject to local state plane and add value field (10)
+open_space <- open_space %>% 
+  st_transform(epsg) %>% 
+  mutate(Value = 10)
+
+# Rasterize to match grid of esa and save raster
+open_space_rast <- open_space %>% 
+  rasterize(esa, 
+            field = "Value",
+            filename = paste0(path, "/open_space_1m.tif"))
+
+
+# Roads -------------------------------------------------------------------
+
+# get roads from OSM
+roads <- opq(bb) %>% 
+  add_osm_feature(key = 'highway',
+                  value = c('motorway', 'trunk', 'primary', 'secondary', 'tertiary', 'residential', 'unclassified', 'motorway_link', 
+                            'trunk_link', 'primary_link', 'secondary_link', 'tertiary_link', 'living_street')) %>% 
+  osmdata_sf() 
+
+
