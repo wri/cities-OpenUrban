0f24eeac9600a3e0896c2508e8aec8af55b86e8e
Work <WRI@Elizabeths-iMac.ht.home>
2023-09-18 13:25:31 -0400

    Select random 10% of buildings from each grid cell

--- Building_samples.R
+++ Building_samples.R
@@ -40,35 +40,89 @@ aois <- tribble(~ city, ~ epsg, ~ zone, ~geoid,
 cities <- cities %>% 
   filter(GEOID10 %in% aois$geoid)
 
-buildings.samp <- tibble()
+build.samp <- tibble()
 
 # Sample per city
-for (city in cities){
+for (i in 1:nrow(cities)){
+  
+  city <- cities %>% 
+    slice(i)
   
   city_bb <- city %>% 
-    st_buffer(dist = 804.672) %>% 
     st_bbox() 
   
-  # Get buildings from OSM & take a random sample of 1000
-  buildings <- opq(city_bb) %>% 
-    add_osm_feature(key = 'building') %>% 
-    osmdata_sf() 
+  # create 10x10 grid over city
+  city_grid <- city_bb %>% 
+    st_make_grid(n = 10, what = "polygons") %>% 
+    st_sf() %>% 
+    st_filter(city) %>% 
+    mutate(ID = row_number())
   
-  buildings <- buildings$osm_polygons %>% 
-    bind_rows(st_cast(buildings$osm_multipolygons, "POLYGON")) 
+  city_samp <- tibble()
   
-  # drop excess fields
-  buildings <- buildings %>% 
-    select(osm_id) %>% 
-    slice_sample(n = 1000) %>% 
-    mutate(city = city$GEOID10)
+  for (x in 1:length(city_grid$ID)){
+    # Buffer grid cell so there will be overlap
+    aoi <- city_grid %>% 
+      filter(ID == x) %>% 
+      st_as_sf() 
+    
+    bb <- st_bbox(aoi)
+    
+    # Get buildings from OSM & take a random sample of 10 buildings from each
+    # grid cell for a total of 1000 per city
+    buildings <- opq(bb) %>% 
+      add_osm_feature(key = 'building') %>% 
+      osmdata_sf() 
+    
+    if (!is.null(nrow(buildings$osm_polygons)) & !is.null(nrow(buildings$osm_multipolygons))){
+      # if there are polygons and multipolygons
+      buildings <- buildings$osm_polygons %>% 
+        bind_rows(st_cast(buildings$osm_multipolygons, "POLYGON")) 
+      
+    } else if (!is.null(nrow(buildings$osm_polygons)) & is.null(nrow(buildings$osm_multipolygons))){
+      # if there are polygons but no multipolygons
+      
+      buildings <- buildings$osm_polygons
+      
+    } else if (is.null(nrow(buildings$osm_polygons)) & !is.null(nrow(buildings$osm_multipolygons))){
+      # if there are multipolygons but no polygons
+      
+      buildings <- st_cast(buildings$osm_multipolygons, "POLYGON")
+      
+    } else {
+      # if there are neither
+      print(paste0("Skipped ", city$NAME10, " grid ", i))
+      next
+    }
+    
+    # drop excess fields
+    buildings <- buildings %>% 
+      select(osm_id) %>% 
+      mutate(city = city$GEOID10)
+    
+    # Sample 10% of buildings. If the area has so few buildings that 10% < 1, 
+    # sample 1 building.
+    if(nrow(buildings) * 0.1 < 1){
+      
+      buildings2 <- buildings %>% 
+        slice_sample(n = 1)
+      
+    } else {
+      
+      buildings2 <- buildings %>% 
+        slice_sample(prop = 0.1)
+      
+    }
+
+    city.samp <- rbind(city.samp, buildings)
+    print(paste0(city$NAME10, " grid ", i))
+    
+  }
   
-  buildings.samp <- bind_rows(buildings.samp, buildings)
+  build.samp <- rbind(city.samp, build.samp)
+  
+  
 }
 
-# Create bounding box for urban area
-# buffered by 804.672 (half a mile in meters)
-# divide into grid for processing
-city_bb <- city %>% 
-  st_buffer(dist = 804.672) %>% 
-  st_bbox() 
+walk(cities, samp)
+
