d2510ec5bf8c3c75a4787a78f5922e570dcead23
Elizabeth Jane Wesley <elizabeth.wesley@wri.org>
2026-05-13 17:16:51 -0400

    reconciled var names for overture release version

--- upload_gee.py
+++ upload_gee.py
@@ -202,7 +202,7 @@ def main(
     city_name,
     gridcell_id,
     version,
-    ovt_release_version,
+    overture_release_version,
     local_file,
     s3_bucket,
     s3_key,
@@ -239,7 +239,7 @@ def main(
         "city": str(city_name),
         "grid_cell": int(gridcell_id) if str(gridcell_id).isdigit() else str(gridcell_id),
         "version": str(version),
-        "overture_release_version": str(ovt_release_version),
+        "overture_release_version": str(overture_release_version),
         "start_time": time.strftime("%Y-%m-%d"),
     }
 
